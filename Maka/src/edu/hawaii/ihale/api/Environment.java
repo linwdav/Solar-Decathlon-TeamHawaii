9a36cef39009b79784793dcd8d53276911638fd8
thgaskell <thgaskell@73f49f84-8fa0-6357-8c54-79e179265a37>
2011-02-03 11:13:29 +0000

    Created Environment structure.

--- Maka/src/edu/hawaii/ihale/api/Environment.java
+++ Maka/src/edu/hawaii/ihale/api/Environment.java
@@ -8,4 +8,24 @@ package edu.hawaii.ihale.api;
  */
 public class Environment {
 
+	/** Unique sensor for GET requests on the Environment. */
+	private Sensor;
+	/** Unique actuator for PUT requests on the Environment. */
+	private Actuator;
+
+	/**
+	 * Polls environment data.
+	 * @return data The current data reading of the Environment.
+	 */
+	public Document getData() {
+		// To be implemented later.
+		return null;
+	}
+	
+	/**
+	 * Issues command to envinronment.
+	 */
+	public void putCommand(Document command) {
+		// To be implemented later.
+	}
 }
