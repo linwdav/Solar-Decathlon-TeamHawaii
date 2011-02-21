be3ac09c52a1e804f58876000a11953c4917e983
nguyen.leonardo@gmail.com <nguyen.leonardo@gmail.com@73f49f84-8fa0-6357-8c54-79e179265a37>
2011-02-21 01:04:22 +0000

    Working on XML representation of SystemStateEntry objects.

---
+++ hoike/src/edu/hawaii/ihale/backend/restserver/TestIHaleServer.java
@@ -0,0 +1,21 @@
+package edu.hawaii.ihale.backend.restserver;
+
+import org.junit.Test;
+
+/**
+ * Unit test of the IHale HTTP server's processing ability.
+ *
+ * @author Leonardo Nguyen
+ * @version Java 1.6.0_21
+ */
+public class TestIHaleServer {
+
+  /**
+   * Test XML parsing and creating an instance of IHaleSystemStateEntryRepresentation.
+   */
+  @Test
+  public void testXML() {
+    // To Remove PMD complaints.
+    System.out.println("");
+  }
+}
