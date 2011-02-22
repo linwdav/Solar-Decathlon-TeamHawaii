1c8220875d4356911f82ee2373b83bc4bf1ecbf0
Bret.K.Ikehara@gmail.com <Bret.K.Ikehara@gmail.com@73f49f84-8fa0-6357-8c54-79e179265a37>
2011-02-22 10:50:32 +0000

    Committing some packages.

---
+++ gecko/src/edu/hawaii/ihale/wicket/model/SystemModel.java
@@ -0,0 +1,30 @@
+package edu.hawaii.ihale.wicket.model;
+
+import java.io.Serializable;
+
+/**
+ * Implements an abstract System Model.
+ * 
+ * @author Bret K. Ikehara
+ */
+public abstract class SystemModel implements Serializable {
+
+  /**
+   * Serial ID.
+   */
+  private static final long serialVersionUID = -3063681993461244149L;
+  protected Long timestamp = 0L;
+  
+  /**
+   * Gets the System Model time stamp.
+   * 
+   * @return Long
+   */
+  public abstract Long getTimestamp();
+  
+  /**
+   * Sets the System Model time stamp.
+   * @param timestamp Long
+   */
+  public abstract void setTimestamp(Long timestamp);  
+}
