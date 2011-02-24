47d52931569b448764b9bf6dee8e4a899ebffa94
linwdav@gmail.com <linwdav@gmail.com@73f49f84-8fa0-6357-8c54-79e179265a37>
2011-02-24 08:38:53 +0000

    Some progress on REST server

--- hoike/src/edu/hawaii/ihale/backend/restserver/IHaleServer.java
+++ hoike/src/edu/hawaii/ihale/backend/restserver/IHaleServer.java
@@ -6,7 +6,9 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 import org.restlet.Application;
+import org.restlet.Component;
 import org.restlet.Restlet;
+import org.restlet.data.Protocol;
 import org.restlet.routing.Router;
 
 /**
@@ -28,6 +30,19 @@ public class IHaleServer extends Application {
   // Contains the mapping of device urls to port numbers as defined in the properties file.
   private static final Map<String, String> uris = new HashMap<String, String>();
   
+  public static void runServer(String contextRoot, int port) throws Exception {
+    // Create a component.
+    Component component = new Component();
+    component.getServers().add(Protocol.HTTP, port);
+    // Create an application (this class).
+    Application application = new IHaleServer();
+    
+    
+    // Attach the application to the component with a defined contextRoot.
+    component.getDefaultHost().attach(contextRoot, application);
+    component.start();
+  }
+  
   /**
    * This main method starts up a web application that will on timed intervals send GET HTTP
    * requests to each system device defined in the properties and on the port connection 
@@ -37,13 +52,27 @@ public class IHaleServer extends Application {
    * @throws Exception If problems occur.
    */
   public static void main(String[] args) throws Exception {
-    
+    readProperties();
+    for (Map.Entry<String, String> entry : uris.entrySet()) {
+      String key = entry.getKey();
+      String contextRoot = "/" + key.split("/")[1] + "/" + key.split("/")[2];
+      System.out.println(contextRoot);
+      //runServer(contextRoot, Integer.valueOf(entry.getValue()));
+    }
+
+    /** TO-DO: Now that we have the mappings we must create a timer method that will at
+     *         intervals send GET requests to each system device on that port connection.
+     *         (i.e., arduino-1.halepilihonua.hawaii.edu/aquaponics/state=7000 was mapped
+     *          as so in the properties file, http://localhost:7000 is representative of
+     *          the URL to Aquaponics system device, Arduino-1.)
+     */
+  }
+  
+  public static void readProperties() {
     try {
       FileInputStream is = new FileInputStream(configFilePath);
       Properties prop = new Properties();
-
       prop.load(is);
-
       String key = "";
       String value = "";
       for (Map.Entry<Object, Object> propItem : prop.entrySet()) {
@@ -58,19 +87,7 @@ public class IHaleServer extends Application {
       System.out.println("failed to read properties file");
       System.out.println(configFilePath);
     }
-    
-    System.out.println(uris);
-
-    /** TO-DO: Now that we have the mappings we must create a timer method that will at
-     *         intervals send GET requests to each system device on that port connection.
-     *         (i.e., arduino-1.halepilihonua.hawaii.edu/aquaponics/state=7000 was mapped
-     *          as so in the properties file, http://localhost:7000 is representative of
-     *          the URL to Aquaponics system device, Arduino-1.)
-     */
   }
-  
-  // NOTE: Since we aren't running a server the overridden method createInboundRoot below
-  //       is no longer necessary.
   
   /**
    * Specify the dispatching restlet that maps URIs to their associated resources for processing.
@@ -79,14 +96,10 @@ public class IHaleServer extends Application {
    */
   @Override
   public Restlet createInboundRoot() {
-
-    // Define the systems that support resource handling by the Restlet HTTP server.
-    String aquaponicsSystem = "aquaponics";
-
     // Create a router restlet.
     Router router = new Router(getContext());
     // Attach the resources to the router.
-    router.attach("/" + aquaponicsSystem + "/{request}", AquaponicsResource.class);
+    router.attach("/aquaponics/{request}", AquaponicsResource.class);
     /**
      * TO-DO: Need to figure out a solution for PV and Electrical since they share the same ending
      * URI patterns. i.e., PV: http://egauge-1.halepilihonua.hawaii.edu/cgi-bin/egauge?tot
