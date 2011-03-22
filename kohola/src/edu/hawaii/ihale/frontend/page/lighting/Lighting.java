44c8d5c5c584722968a173c948e9252e54d348a8
Kylan.Hughes <Kylan.Hughes@73f49f84-8fa0-6357-8c54-79e179265a37>
2011-03-22 19:45:16 +0000

    made some updates to lighting page.  Got drop-down choices to work with wicket, set up structure for later work.

--- kohola/src/edu/hawaii/ihale/frontend/page/lighting/Lighting.java
+++ kohola/src/edu/hawaii/ihale/frontend/page/lighting/Lighting.java
@@ -1,5 +1,14 @@
 package edu.hawaii.ihale.frontend.page.lighting;
 
+import java.util.Arrays;
+import java.util.List;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.model.PropertyModel;
+import edu.hawaii.ihale.api.ApiDictionary.IHaleRoom;
+import edu.hawaii.ihale.frontend.SolarDecathlonApplication;
 import edu.hawaii.ihale.frontend.SolarDecathlonSession;
 import edu.hawaii.ihale.frontend.page.Header;
 
@@ -15,13 +24,150 @@ public class Lighting extends Header {
   /** Support serialization. */
   private static final long serialVersionUID = 1L;
 
+  private String selectedRoom = ((SolarDecathlonSession) getSession()).getLightingSession()
+      .getRoom();
+  private boolean livingState = false;
+  private boolean diningState = false;
+  private boolean kitchenState = false;
+  private boolean bathroomState = false;
+
+  private String buttonOn = "green-button";
+  private String buttonOff = "gray-button";
+
   /**
    * Layout of page.
    */
-  public Lighting() {    
+  public Lighting() {
     // More to come
-    ((SolarDecathlonSession)getSession()).getHeaderSession().setActiveTab(3);
+    ((SolarDecathlonSession) getSession()).getHeaderSession().setActiveTab(3);
 
+//    setLivingState();
+//    setDiningState();
+//    setKitchenState();
+//    setBathroomState();
+
+    Form<String> form = new Form<String>("form");
+    WebMarkupContainer wmcRoom = new WebMarkupContainer("room-wmc");
+    form.add(wmcRoom);
+    wmcRoom.setVisible(true);
+    DropDownChoice<String> roomChoices =
+        new DropDownChoice<String>("room", new PropertyModel<String>(
+        ((SolarDecathlonSession) getSession()).getLightingSession(), "room"),
+        ((SolarDecathlonSession) getSession()).getLightingSession().getRooms()) {
+      
+      private static final long serialVersionUID = 1L;
+      
+      /**
+       * For when user chooses new room.
+       */
+      @Override
+      protected void onSelectionChanged(String newSelection) {
+        if ("Living Room".equals(newSelection)) {
+          //set button to living state
+          //set intensity to living state
+          //set color to living state
+        }
+        else if ("Dining Room".equals(newSelection)) {
+          //set button to dining state
+          //set intensity to dining state
+          //set color to dining state
+        }
+        else if ("Kitchen".equals(newSelection)) {
+          //set button to kitchen state
+          //set intensity to kitchen state
+          //set color to kitchen state
+        }
+        else if ("Bathroom".equals(newSelection)) {
+          //set button to bathroom state
+          //set intensity to bathroom state
+          //set color to bathroom state
+        }
+        //for "Choose One" option
+        else {
+          // make it just lighting or disable options?
+        }
+      }
+    };
+    wmcRoom.add(roomChoices.setRequired(true));
+
+    // the on button
+    Link<String> onButton = new Link<String>("OnButton") {
+      private static final long serialVersionUID = 1L;
+
+      @Override
+      /**
+       * Turn on the light in this room.
+       */
+      public void onClick() {
+        System.out.println("Lights on");
+        // do command here
+      }
+    };
+    wmcRoom.add(onButton);
+
+    // the off button
+    Link<String> offButton = new Link<String>("OffButton") {
+      private static final long serialVersionUID = 1L;
+
+      @Override
+      /**
+       * Turn off the light in this room.
+       */
+      public void onClick() {
+        System.out.println("Lights off");
+        // do command here
+      }
+    };
+    wmcRoom.add(offButton);
+    add(form);
   }
+
+  /**
+   * Sets the state of the living room based on what is in repository.
+   */
+//  private void setLivingState() {
+//    if (SolarDecathlonApplication.getRepository().getLightingEnabled(IHaleRoom.LIVING).getValue() == true) {
+//      livingState = true;
+//    }
+//    else {
+//      livingState = false;
+//    }
+//  }
+
+  /**
+   * Sets the state of the dining room based on what is in repository.
+   */
+//  private void setDiningState() {
+//    if (SolarDecathlonApplication.getRepository().getLightingEnabled(IHaleRoom.DINING).getValue() == true) {
+//      diningState = true;
+//    }
+//    else {
+//      diningState = false;
+//    }
+//  }
+
+  /**
+   * Sets the state of the kitchen room based on what is in repository.
+   */
+//  public void setKitchenState() {
+//    if (SolarDecathlonApplication.getRepository().getLightingEnabled(IHaleRoom.KITCHEN).getValue() == true) {
+//      kitchenState = true;
+//    }
+//    else {
+//      kitchenState = false;
+//    }
+//  }
+
+  /**
+   * Sets the state of the bathroom based on what is in repository.
+   */
+//  public void setBathroomState() {
+//    if (SolarDecathlonApplication.getRepository().getLightingEnabled(IHaleRoom.BATHROOM).getValue() == true) {
+//      bathroomState = true;
+//    }
+//    else {
+//      bathroomState = false;
+//    }
+//  }
 
 }
--- kohola/src/edu/hawaii/ihale/frontend/page/lighting/LightingSession.java
+++ kohola/src/edu/hawaii/ihale/frontend/page/lighting/LightingSession.java
@@ -1,6 +1,9 @@
 package edu.hawaii.ihale.frontend.page.lighting;
 
 import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * The lighting sessions page.
@@ -12,12 +15,44 @@ public class LightingSession implements Serializable {
 
   /** Support serialization. */
   private static final long serialVersionUID = 1L;
-  
+
+  private String room;
+
+  private List<String> rooms = Arrays.asList(new String[] { "Living Room", "Dining Room",
+      "Kitchen", "Bathroom" });
+
   /**
    * empty for now.
    */
   public LightingSession() {
-    //empty
+
+    // empty
+  }
+
+  /**
+   * Returns the room associated with the user's session.
+   * 
+   * @return The room the user is viewing in lighting page.
+   */
+  public String getRoom() {
+    return room;
+  }
+
+  /**
+   * Sets the room to the room the user is currently viewing.
+   * 
+   * @param room - the room the user is looking at in the application.
+   */
+  public void setRoom(String room) {
+    this.room = room;
   }
   
+  /**
+   * Returns a list of the rooms in the house.
+   * 
+   * @return The names of the rooms in a list.
+   */
+  public List<String> getRooms() {
+    return Collections.unmodifiableList(rooms);
+  }
 }
