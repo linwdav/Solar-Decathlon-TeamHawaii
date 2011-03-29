14be0a65adf79354de34edadcfad0c24bfdeae2e
michaelicera@gmail.com <michaelicera@gmail.com@73f49f84-8fa0-6357-8c54-79e179265a37>
2011-03-29 21:48:16 +0000

    [Back-End] Cleaned up code a bit.

--- hoike2/src/edu/hawaii/ihale/backend/TestXmlHandler.java
+++ hoike2/src/edu/hawaii/ihale/backend/TestXmlHandler.java
@@ -1,6 +1,7 @@
 package edu.hawaii.ihale.backend;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import java.io.IOException;
 import javax.xml.xpath.XPathExpressionException;
 import org.junit.BeforeClass;
@@ -15,11 +16,6 @@ import org.restlet.representation.FileRepresentation;
  */
 public class TestXmlHandler {
 
-  private static String xmlDirectory = System.getProperty("user.dir")
-      + "/src/edu/hawaii/ihale/backend/xml/test/";
-  private static String eGaugeXml = xmlDirectory + "test-egauge.xml";
-  private static String historyXml = xmlDirectory + "history.xml";
-
   private static FileRepresentation historyRepresentation;
   private static FileRepresentation eGaugeRepresentation;
   private static XmlHandler handle = new XmlHandler();
@@ -29,6 +25,12 @@ public class TestXmlHandler {
    */
   @BeforeClass
   public static void xmltoRepresentation() {
+
+    String xmlDirectory =
+        System.getProperty("user.dir") + "/src/edu/hawaii/ihale/backend/xml/test/";
+    String eGaugeXml = xmlDirectory + "test-egauge.xml";
+    String historyXml = xmlDirectory + "history.xml";
+
     historyRepresentation = new FileRepresentation(historyXml, MediaType.TEXT_XML);
     eGaugeRepresentation = new FileRepresentation(eGaugeXml, MediaType.TEXT_XML);
   }
@@ -43,11 +45,11 @@ public class TestXmlHandler {
       testPass = handle.xml2StateEntry(historyRepresentation);
     }
     catch (XPathExpressionException e) {
-      // TODO Auto-generated catch block
+      fail("XPathExpressionException");
       e.printStackTrace();
     }
     catch (IOException e) {
-      // TODO Auto-generated catch block
+      fail("IOException");
       e.printStackTrace();
     }
     assertTrue(testPass);
@@ -63,11 +65,11 @@ public class TestXmlHandler {
       testPass = handle.eGuage2StateEntry(eGaugeRepresentation);
     }
     catch (XPathExpressionException e) {
-      // TODO Auto-generated catch block
+      fail("XPathExpressionException");
       e.printStackTrace();
     }
     catch (IOException e) {
-      // TODO Auto-generated catch block
+      fail("IOException");
       e.printStackTrace();
     }
     assertTrue(testPass);
