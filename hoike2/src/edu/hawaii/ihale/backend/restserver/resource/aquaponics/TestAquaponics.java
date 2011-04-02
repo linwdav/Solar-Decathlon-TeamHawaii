26c68a0596b0612045925795803ad4c0e947538e
michaelicera@gmail.com <michaelicera@gmail.com@73f49f84-8fa0-6357-8c54-79e179265a37>
2011-04-02 19:32:28 +0000

    [Back-End] Finished JUnit tests for Aquaponics.

--- hoike2/src/edu/hawaii/ihale/backend/restserver/resource/aquaponics/TestAquaponics.java
+++ hoike2/src/edu/hawaii/ihale/backend/restserver/resource/aquaponics/TestAquaponics.java
@@ -2,6 +2,7 @@ package edu.hawaii.ihale.backend.restserver.resource.aquaponics;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import java.io.IOException;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.restlet.ext.xml.DomRepresentation;
@@ -11,7 +12,7 @@ import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import edu.hawaii.ihale.api.ApiDictionary.IHaleState;
 import edu.hawaii.ihale.api.ApiDictionary.IHaleSystem;
-import edu.hawaii.ihale.api.repository.impl.Repository; 
+import edu.hawaii.ihale.api.repository.impl.Repository;
 import edu.hawaii.ihale.backend.restserver.RestServer;
 import edu.hawaii.ihale.backend.restserver.resource.SystemData;
 
@@ -65,7 +66,7 @@ public class TestAquaponics {
   }
 
   /**
-   * Tests put command with aquaponics. Command: SET_TEMPERATURE; Arg: 25. Won't work until we test
+   * Tests PUT command with aquaponics. Command: SET_TEMPERATURE; Arg: 25. Won't work until we test
    * with a simulator.
    * 
    * @throws Exception Thrown if server fails to run.
@@ -73,13 +74,53 @@ public class TestAquaponics {
   @Ignore
   @Test
   public void testPut() throws Exception {
+
     Repository repository = new Repository();
+
+    // Run the REST server.
     RestServer.runServer(8111);
+
+    // Send PUT command to server.
     String uri = "http://localhost:8111/AQUAPONICS/command/SET_TEMPERATURE?arg=25";
     ClientResource client = new ClientResource(uri);
     client.put(uri);
 
-    assertEquals("Checking sent argument", repository
-        .getAquaponicsTemperatureCommand().getValue(), Integer.valueOf(25));
+    assertEquals("Checking sent argument", Integer.valueOf(25), repository
+        .getAquaponicsTemperatureCommand().getValue());
+  }
+
+  /**
+   * Tests GET command with aquaponics. Won't work until we test with a simulator.
+   * 
+   * @throws IOException Thrown if Document creation fails.
+   */
+  @Ignore
+  @Test
+  public void testGet() throws IOException {
+
+    // Send GET command to server to retrieve XML of the current state.
+    String uri = "http://localhost:8111/AQUAPONICS/state";
+    ClientResource client = new ClientResource(uri);
+
+    DomRepresentation stateRepresentation = new DomRepresentation(client.get());
+    Document stateDocument = stateRepresentation.getDocument();
+
+    // Retrieve system name from XML.
+    String rootNodeName = stateDocument.getFirstChild().getNodeName();
+
+    assertEquals("Checking that this is XML for the current state.", "state-data", rootNodeName);
+
+    // Send GET command to server to retrieve XML of the state history.
+    uri = "http://localhost:8111/AQUAPONICS/state?since=1";
+    client = new ClientResource(uri);
+
+    stateRepresentation = new DomRepresentation(client.get());
+    stateDocument = stateRepresentation.getDocument();
+
+    // Retrieve system name from XML.
+    rootNodeName = stateDocument.getFirstChild().getNodeName();
+
+    assertEquals("Checking that this is XML for the state history.", "state-history", rootNodeName);
+
   }
 }
