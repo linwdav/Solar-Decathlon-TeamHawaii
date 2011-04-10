134d2a530b6a0179958b5c364e87d1e72e4f11aa
kgleong@gmail.com <kgleong@gmail.com@73f49f84-8fa0-6357-8c54-79e179265a37>
2011-04-10 15:08:08 +0000

    Adds code to empty method.

--- kohola/src/edu/hawaii/systemh/frontend/weatherparser/TestWeather.java
+++ kohola/src/edu/hawaii/systemh/frontend/weatherparser/TestWeather.java
@@ -1,7 +1,11 @@
 package edu.hawaii.systemh.frontend.weatherparser;
 
 
+
+import static org.junit.Assert.assertTrue;
+
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 import edu.hawaii.ihale.api.command.IHaleCommand;
 import edu.hawaii.systemh.frontend.BlankBackend;
@@ -13,6 +17,13 @@ public class TestWeather {
 	public static void setUpBeforeClass() throws Exception {
 		IHaleCommand testingBackend = new BlankBackend();
 		SolarDecathlonApplication.setBackend(testingBackend);
+	}
+	
+	@Test
+	public void testWeatherModule() {
+		WeatherParser parser = new WeatherParser("Honolulu");
+	    String currentWeather = parser.getCurrentWeather().toString();
+	    assertTrue("Check Weather", currentWeather != "");
 	}
 
 }
