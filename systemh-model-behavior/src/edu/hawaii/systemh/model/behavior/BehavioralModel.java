d4f9d319ff702952b180521657c05063243e4ace
thgaskell <thgaskell@73f49f84-8fa0-6357-8c54-79e179265a37>
2011-04-15 11:23:13 +0000

    Initial commit.

---
+++ systemh-model-behavior/src/edu/hawaii/systemh/model/behavior/BehavioralModel.java
@@ -0,0 +1,29 @@
+package edu.hawaii.systemh.model.behavior;
+
+/**
+ * Improve the realism of the house simulation by creating a 
+ * more sophisticated "behavioral model" of house.
+ * This should take into account weather, time of day, 
+ * and expected occupant behaviors to do things like:
+ * Change inside/outside temperatures.
+ * Turn on or off various aquaponics systems.
+ * Maybe indicate that fish should be fed or whatever.
+ * If outside temperature is hot, then water temperature should increase, etc.
+ * HVAC use and/or thermostat setting should result in automated on or off of system. 
+ * PV generation should depend upon time of day and outside weather (cloudy, sunny). 
+ * 
+ * @author Gregory Burgess
+ * @author Tony Gaskell
+ * @author Kurt Teichman
+ */
+public class BehavioralModel {
+
+  /**
+   * Sets up the REST server to communicate with the IHaleBackend object.
+   * 
+   * @param args (ignored)
+   */
+  public static void main(String[] args) {
+    System.err.println("Behavioral model of the house.");
+  }
+}
