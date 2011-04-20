dc4700afa9bc6f715f4ad20b0ed63f31957fb06e
kgleong@gmail.com <kgleong@gmail.com@73f49f84-8fa0-6357-8c54-79e179265a37>
2011-04-20 08:34:27 +0000

    Added Week and 2-month axis generation for GoogleCharts.  Verify Passed.

--- systemh-frontend/src/edu/hawaii/systemh/frontend/googlecharts/ChartDataHelper.java
+++ systemh-frontend/src/edu/hawaii/systemh/frontend/googlecharts/ChartDataHelper.java
@@ -1,9 +1,13 @@
 package edu.hawaii.systemh.frontend.googlecharts;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Locale;
 
 /**
  * Organizes data from the System-H repository to integrate with the Wicket-GoogleCharts API.
+ * This class makes the following assumption: that data will be looked at going backwards
+ * from a given date.  E.g., a month before today, a week before today, 24 hours before today.
  * 
  * @author Kevin Leong
  */
@@ -14,12 +18,17 @@ public class ChartDataHelper {
   static final long oneDayInMillis = 1000L * 60L * 60L * 24L;
   static final long oneMonthInMillis = 1000L * 60L * 60L * 24L * 28L;
   static final long oneHourInMillis = 1000L * 60L * 60L;
+  
+  // Basic conversions for time intervals
+  static final int HOURS_IN_A_DAY = 24;
+  static final int DAYS_IN_A_WEEK = 7;
+  static final int WEEKS_IN_A_MONTH = 4;
 
   // String array holding the axis labels
   String[] axis;
 
   // String array holding list of days for week chart
-  // String[] weekList = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+  String[] weekList = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
   String[] dayList = { "12AM", "", "2", "", "4", "", "6", "", "8", "", "10", "", "12PM", "", "2",
       "", "4", "", "6", "", "8", "", "10", "" };
 
@@ -41,7 +50,8 @@ public class ChartDataHelper {
   } // End constructor
 
   /**
-   * Sets the axis labels.
+   * Sets the axis labels given a specified time interval and
+   * current date.
    * 
    * @param interval The time interval. E.g., Day, Week, Month.
    * @param calendar The date to start at. E.g., current date.
@@ -55,13 +65,13 @@ public class ChartDataHelper {
       int startingHour = calendar.get(Calendar.HOUR_OF_DAY);
 
       // Create a list to hold the axis labels
-      String[] actualDayList = new String[24];
+      String[] actualDayList = new String[HOURS_IN_A_DAY];
 
-      // Loop through to popoulate day list.
-      for (int i = 23; i >= 0; i--) {
+      // Loop through to populate day list.
+      for (int i = (HOURS_IN_A_DAY - 1); i >= 0; i--) {
         actualDayList[i] = dayList[startingHour--];
         if (startingHour < 0) {
-          startingHour = 23;
+          startingHour = (HOURS_IN_A_DAY - 1);
         }
       } // End for loop
 
@@ -71,18 +81,54 @@ public class ChartDataHelper {
 
     case WEEK:
       // Get the day for the date given 1 = Sunday, 7 = Saturday
-      // int startingDay = calendar.get(Calendar.DAY_OF_WEEK);
-
+      // Subtract 1 since week starts at 1.
+      int startingDay = calendar.get(Calendar.DAY_OF_WEEK) - 1;
+      
+      // Create list to hold axis labels
+      String[] actualWeekList = new String[DAYS_IN_A_WEEK];
+      
+      // Loop through to populate week list.
+      for (int i = (DAYS_IN_A_WEEK - 1); i >= 0; i--) {
+        actualWeekList[i] = weekList[startingDay--];
+        if (startingDay < 0) {
+          startingDay = (DAYS_IN_A_WEEK - 1);
+        }
+      } // End for loop
+      
+      // Set axis label to week axis
+      this.axis = actualWeekList;
+      break;
+      
     case MONTH:
+      // Create list for 8-weeks worth of axis labels
+      String[] actualMonthList = new String[WEEKS_IN_A_MONTH * 2];
+      
+      // Set Format for date.  e.g., Apr 19
+      String dateFormat = "MMM dd";
+      SimpleDateFormat formatter = new SimpleDateFormat(dateFormat, Locale.US);
+      Calendar tempCalendar = calendar;
+      
+      // Back off by 2 months.  This is to make loop code below more readable.
+      tempCalendar.add(Calendar.WEEK_OF_YEAR, (-1 * WEEKS_IN_A_MONTH * 2));
+      
+      // Loop through and populate array with 8-weeks of dates.
+      for (int i = 0; i < (WEEKS_IN_A_MONTH * 2); i++) {
+        tempCalendar.add(Calendar.WEEK_OF_YEAR, 1);
+        actualMonthList[i] = formatter.format(tempCalendar.getTime());
+      }
+      // Set axis label to month axis.
+      this.axis = actualMonthList;
+      break;
+      
     default:
       break;
     } // End Switch
   } // End set axis
 
   /**
-   * Returns the contents of a string array.
+   * Returns the contents of the axis array.
    * 
-   * @param size The size of the array.
+   * @param size The size of the axis array.
    * @return The string containing the contents.
    */
   public String stringArrayToString(int size) {
--- systemh-frontend/src/edu/hawaii/systemh/frontend/googlecharts/ChartDataHelperTest.java
+++ systemh-frontend/src/edu/hawaii/systemh/frontend/googlecharts/ChartDataHelperTest.java
@@ -50,21 +50,42 @@ public class ChartDataHelperTest {
     try {
       // Convert date to a calendar object
       Date date = formatter.parse(date01);
-      Calendar cal = Calendar.getInstance();
-      cal.setTime(date);
+      Calendar calendar = Calendar.getInstance();
+      calendar.setTime(date);
 
       // Set the axis for the interval DAY using the given date.
-      cdh.setAxis(TimeInterval.DAY, cal);
+      cdh.setAxis(TimeInterval.DAY, calendar);
 
       // Test to make sure axis matches what is expected.
       String actualAxis = cdh.stringArrayToString(24);
+      
+      // Should start at 2AM
       String expectedAxis = "2  4  6  8  10  12PM  2  4  6  8  10  12AM  ";
       assertEquals("Day Test", actualAxis, expectedAxis);
-    }
+      
+      // Set the axis for the interval WEEK using the given date.
+      cdh.setAxis(TimeInterval.WEEK, calendar);
+
+      // Test to make sure axis matches what is expected.
+      // Should start at Wed
+      actualAxis = cdh.stringArrayToString(7);
+      expectedAxis = "Wed Thu Fri Sat Sun Mon Tue ";      
+      assertEquals("Week Test", actualAxis, expectedAxis);
+      
+      // Set the axis for the interval WEEK using the given date.
+      cdh.setAxis(TimeInterval.MONTH, calendar);
+
+      // Test to make sure axis matches what is expected.
+      // Should start at Wed
+      actualAxis = cdh.stringArrayToString(8);
+      expectedAxis = "Mar 01 Mar 08 Mar 15 Mar 22 Mar 29 Apr 05 Apr 12 Apr 19 ";      
+      assertEquals("Month Test", actualAxis, expectedAxis);
+
+    } // End try block
     catch (ParseException e) {
       System.out.println("Problem parsing date.");
       e.printStackTrace();
-    }
+    } // End catch block
   } // End testSetAxis
 
 } // End ChartDataHelperTest class
