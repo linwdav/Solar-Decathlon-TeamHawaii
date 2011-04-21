0d2f5fdcf922a01e54883998ce6b41096ed8e5cd
linwdav@gmail.com <linwdav@gmail.com@73f49f84-8fa0-6357-8c54-79e179265a37>
2011-04-21 19:26:42 +0000

    Control Wicket logging output.  Changed the logging framework from slf4j-simple to slf4j-jdk14.  Also updated the version from 1.5.6 to 1.6.1

--- systemh-frontend/src/edu/hawaii/systemh/frontend/Jetty.java
+++ systemh-frontend/src/edu/hawaii/systemh/frontend/Jetty.java
@@ -1,5 +1,12 @@
 package edu.hawaii.systemh.frontend;
 
+import java.io.FileInputStream;
+import java.util.logging.Handler;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+import java.util.logging.FileHandler;
+import java.io.IOException;
+import java.util.logging.Level;
 import org.apache.wicket.protocol.http.WicketServlet;
 import org.mortbay.jetty.Server;
 import org.mortbay.jetty.servlet.Context;
@@ -33,6 +40,26 @@ public class Jetty {
    * @throws Exception If things go wrong.
    */
   public static void main(String[] args) throws Exception {
+    
+    // Log actions into a text file
+    String currentDirectory = System.getProperty("user.dir");
+    String configurationFilename = "logging.properties";
+    String configFilePath = currentDirectory + "/" + configurationFilename;
+    try {
+      FileInputStream config = new FileInputStream(configFilePath);
+      LogManager.getLogManager().readConfiguration(config);
+      String logFilename = System.getProperty("user.home") + "/.ihale/log.txt";
+      // Allow appending to the logging file.
+      Handler fh = new FileHandler(logFilename, true);
+      Logger.getLogger("").setLevel(Level.OFF);
+      Logger.getLogger("").addHandler(fh);
+    }
+    catch (IOException ioe) {
+      // CheckStyle was complaining about use of tabs when there wasn't so this long string is
+      // placed into a String variable to comply with the warning.
+      System.out.println("Error, logging properties file not found at " + configFilePath);
+      System.out.println("Log messages will be appended to the console");
+    }
     
     Server server = new Server(port);
     Context context = new Context(server, "/" + contextPath, Context.SESSIONS);
