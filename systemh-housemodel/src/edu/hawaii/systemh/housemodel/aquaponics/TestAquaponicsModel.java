bb344d844dc3605c812f30870e66a57a67d67f90
kurt.t.teichman <kurt.t.teichman@73f49f84-8fa0-6357-8c54-79e179265a37>
2011-05-01 23:34:36 +0000

    test cases for the respective systems

---
+++ systemh-housemodel/src/edu/hawaii/systemh/housemodel/aquaponics/TestAquaponicsModel.java
@@ -0,0 +1,58 @@
+package edu.hawaii.systemh.housemodel.aquaponics;
+
+import static org.junit.Assert.assertEquals;
+import java.util.List;
+import org.junit.Test;
+import edu.hawaii.systemh.api.repository.TimestampDoublePair;
+import edu.hawaii.systemh.housemodel.EnergyConsumptionModel;
+import edu.hawaii.systemh.housemodel.EnergyConsumptionDictionary.EnergyConsumptionDevice;
+import edu.hawaii.systemh.housemodel.EnergyConsumptionDictionary.EnergyConsumptionSystem;
+
+/**
+ * Tests the HTTP operations of the system.
+ * 
+ * @author Kurt Teichman
+ */
+public class TestAquaponicsModel {
+  
+  /**
+   * Tests that the values put into the Aquaponics Consumption Model are working
+   * as intended.
+   */
+  @Test
+  public void testAquaponicsValues() throws Exception {
+    
+    EnergyConsumptionModel energyConsumptionModel = new EnergyConsumptionModel();
+    java.util.Date todayDate = new java.util.Date();
+    
+    long startTime = todayDate.getTime();
+    long endTime = startTime + 60*60*24*7; // 1 week from our startTime
+
+    List<TimestampDoublePair> aquaponicsSystemData =
+      energyConsumptionModel.getSystemLoadDuringInterval(
+          EnergyConsumptionSystem.AQUAPONICS, startTime, endTime);
+    
+    List<TimestampDoublePair> waterPumpValues = 
+      energyConsumptionModel.getDeviceLoadDuringInterval(
+          EnergyConsumptionDevice.WATER_PUMP, startTime, endTime);
+    
+    List<TimestampDoublePair> heaterValues =
+      energyConsumptionModel.getDeviceLoadDuringInterval(
+          EnergyConsumptionDevice.AQUAPONICS_HEATER, startTime, endTime);
+    
+    List<TimestampDoublePair> filterValues =
+      energyConsumptionModel.getDeviceLoadDuringInterval(
+          EnergyConsumptionDevice.WATER_FILTER, startTime, endTime);
+    
+    double systemTotal = 0, waterPumpTotal = 0, heaterTotal = 0, filterTotal = 0;
+
+    for (int i = 0; i < aquaponicsSystemData.size(); i++) {
+      systemTotal += aquaponicsSystemData.get(i).getValue();
+      waterPumpTotal += waterPumpValues.get(i).getValue();
+      heaterTotal += heaterValues.get(i).getValue();
+      filterTotal += filterValues.get(i).getValue();
+    }
+    
+    assertEquals(systemTotal, (waterPumpTotal + heaterTotal + filterTotal), 0.0);
+  }
+}
