7db340ecaafa55119a06447ca87d071fc9bc533a
kgleong@gmail.com <kgleong@gmail.com@73f49f84-8fa0-6357-8c54-79e179265a37>
2011-05-12 02:52:09 +0000

    Initial Commit of SystemH-iOS project

---
+++ systemh-ios/SystemH/SystemH/SystemHAppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  SystemHAppDelegate.h
+//  SystemH
+//
+//  Created by leong on 5/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SystemHAppDelegate : NSObject <UIApplicationDelegate> {
+    
+    
+    UITabBarController *tabBarController;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet UITabBarController *tabBarController;
+
+@end
