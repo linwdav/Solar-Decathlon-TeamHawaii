4db1b62831b68637262511e394e64613c9b2637e
kgleong@gmail.com <kgleong@gmail.com@73f49f84-8fa0-6357-8c54-79e179265a37>
2011-05-13 23:50:23 +0000

    Started on HVAC Page

---
+++ systemh-ios/SystemH/SystemH/HvacController.h
@@ -0,0 +1,16 @@
+//
+//  HvacController.h
+//  SystemH
+//
+//  Created by leong on 5/13/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface HvacController : UIViewController {
+    
+}
+
+@end
