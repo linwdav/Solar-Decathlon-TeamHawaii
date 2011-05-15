866afba8b94fc8b0294c0e5c35cf6c8b4ad74736
kgleong@gmail.com <kgleong@gmail.com@73f49f84-8fa0-6357-8c54-79e179265a37>
2011-05-15 18:25:09 +0000

    Update to naming of files.  Lighting page started.

---
+++ systemh-ios/SystemH/SystemH/LightingController.h
@@ -0,0 +1,16 @@
+//
+//  LightingController.h
+//  SystemH
+//
+//  Created by leong on 5/15/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface LightingController : UIViewController {
+    
+}
+
+@end
